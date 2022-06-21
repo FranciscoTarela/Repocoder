a02c0c741b67bb1256685b45f3737944f9ebf12d
Mavok <tarela12@gmail.com>
2022-06-21 19:57:29 -0300

    bkp commit

---
+++ script2.py
@@ -0,0 +1,10 @@
+import sys
+
+print(f"hola {sys.argv[1]}")
+
+
+if len(sys.argv) != 3:
+    print("Error necesito 2 argumentos")
+else:
+    for i in range(int(sys.argv[2])):
+        print(sys.argv[1])
