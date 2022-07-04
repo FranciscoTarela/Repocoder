b8bd472ca93abfbb8f0b492dc80619540d20c8bc
FranciscoTarela <107963936+FranciscoTarela@users.noreply.github.com>
2022-07-04 19:32:27 -0300

    Delete Proyecto_Familiar directory

--- Proyecto_Familiar/Familia_APP/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from datetime import datetime, date
-
-class Familiar(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    dni = models.IntegerField()
-    fecha = models.DateField()
